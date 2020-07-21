8e9d61e3e43e384062822800b7871abe6d038647
Hidemitsu Aoki <olympos0117@gmail.com>
2020-07-21 18:24:20 +0900

    Add validation and association to Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,14 @@
 class Event < ApplicationRecord
+  has_many :participation_events, dependent: :destroy
+  has_many :tag_events, dependent: :destroy
+
+  belongs_to :user
+  belongs_to :community
+
+  validates :title, presence: true, length: {maximum: 20, minimum: 2}
+  validates :start_date, presence: true
+  validates :start_time, presence: true
+  validates :place, presence: true
+  validates :user_id, presence: true
+
 end
