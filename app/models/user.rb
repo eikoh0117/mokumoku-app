8fb38184bea6bd7e2974538f2996be9420da5700
Hidemitsu Aoki <olympos0117@gmail.com>
2020-07-21 18:15:35 +0900

    Add validation and association to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :events, dependent: :destroy
+  has_many :participation_events, dependent: :destroy
+  has_many :communities, dependent: :restrict_with_error
+  has_many :participation_communities, dependent: :destroy
+
+  validates :name, presence: true, length: {maximum: 10, minimum: 2}
+  validates :email, presence: true
+  validates :uid, presence: true
 end
