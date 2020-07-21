f9667009715deab78c2c1a8907845b2244675ff1
Hidemitsu Aoki <olympos0117@gmail.com>
2020-07-21 18:37:05 +0900

    Add validation and association to ParticipationEvent

--- app/models/participation_event.rb
+++ app/models/participation_event.rb
@@ -1,2 +1,9 @@
 class ParticipationEvent < ApplicationRecord
+  belongs_to :user
+  belongs_to :event
+
+  validates :user_id, presence: true
+  validates :event_id, presence: true
+  validates :purpose, presence: true, length: {maximum: 200}
+  validates :achivement, presence: true, length: {maximum: 200}
 end
