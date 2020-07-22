4a3a0f22bc56fd9afdc6946613b631098450512a
Hidemitsu Aoki <olympos0117@gmail.com>
2020-07-22 14:15:13 +0900

    Modify user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email
 
   has_many :events
-  has_many :communities
-  has_many :participation_events
-  has_many :participation_communities
+  # has_many :communities
+  # has_many :participation_events
+  # has_many :participation_communities
 end
