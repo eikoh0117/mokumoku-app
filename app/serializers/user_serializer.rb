1ea5da48cfd2e3480fe21a3f827ac4f43ee2d3f4
Hidemitsu Aoki <olympos0117@gmail.com>
2020-07-22 13:39:55 +0900

    Generate user_serializer and config arrtibutes, association

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,8 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :name, :email
+
+  has_many :events
+  has_many :communities
+  has_many :participation_events
+  has_many :participation_communities
+end
