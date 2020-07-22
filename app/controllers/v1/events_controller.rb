da7c14eeebf1e8381efa5501a497af37986a779f
Hidemitsu Aoki <olympos0117@gmail.com>
2020-07-22 13:55:16 +0900

    Generate events_controller and add 2 actions

---
+++ app/controllers/v1/events_controller.rb
@@ -0,0 +1,22 @@
+class V1::EventsController < ApplicationController
+    def create
+      event = Event.new(event_params)
+      if event.save
+        render json: event, status: :created
+      else
+        render json: event.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+        event = event.find(params[:id])
+        if event.destroy
+            render json: event
+        end
+    end
+
+    private
+      def event_params
+        params.require(:event).permit(:title, :detail, :start_date, :start_time, :end_date, :end_time, :place, :community_id, :user_id, :is_private)
+      end
+end
