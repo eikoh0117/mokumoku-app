1a6a0d92622d152cc7202d07727bdcfbc356cdc2
Hidemitsu Aoki <olympos0117@gmail.com>
2020-07-25 02:08:14 +0900

    Add index to resources of events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    resources :events, only: [:create, :destroy]
+    resources :events, only: [:index, :create, :destroy]
     resources :users, only: [:index, :create]
   end
 end
