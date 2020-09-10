4142b14e87bbd936b638f5b5bcca30fdf9249bcc
Hidemitsu Aoki <olympos0117@gmail.com>
2020-09-11 03:43:37 +0900

    Add index action to participation_events_controller

--- app/controllers/v1/participation_events_controller.rb
+++ app/controllers/v1/participation_events_controller.rb
@@ -8,9 +8,17 @@ class V1::ParticipationEventsController < ApplicationController
     end
   end
   def destroy
-    participation_event = ParticipationEvent.find(params[:id])
+    user = User.find(params[:user_id])
+    event = Event.find(params[:id])
+    participation_event = ParticipationEvent.find_by(event_id: event.id, user_id: user.id)
     if participation_event.destroy
       render json: participation_event
+    end
+  end
+  def index
+    if params[:uid]
+      participation_events = ParticipationEvent.where(user_id: params[:uid])
+      render json: participation_events
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :events, only: [:index, :create, :destroy]
     resources :users, only: [:index, :create]
-    resources :participation_events, only: [:create, :destroy]
+    resources :participation_events, only: [:create, :destroy, :index]
   end
 end
