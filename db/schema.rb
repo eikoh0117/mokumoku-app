ee9f8c101d354647ea9a782f37e8ad1f78e3a20d
Hidemitsu Aoki <olympos0117@gmail.com>
2020-09-12 18:54:56 +0900

    Modify events migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_061437) do
+ActiveRecord::Schema.define(version: 2020_09_12_093313) do
 
   create_table "communities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -24,10 +24,8 @@ ActiveRecord::Schema.define(version: 2020_07_21_061437) do
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.text "detail"
-    t.date "start_date", null: false
-    t.time "start_time", null: false
-    t.date "end_date"
-    t.time "end_time"
+    t.datetime "start_time", null: false
+    t.datetime "end_time"
     t.string "place", null: false
     t.bigint "user_id", null: false
     t.bigint "community_id"
