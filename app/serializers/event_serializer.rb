c5007a2f21c0e3a416c6fdeebc166c60181cf80d
Hidemitsu Aoki <olympos0117@gmail.com>
2020-09-13 11:13:35 +0900

    Delete unnecessary attributes

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,8 +1,8 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :title, :detail, :start_date, :start_time, :end_date, :end_time, :place, :hostname #, :community_id, :is_private, :tag_names, :participation_users_name
+  attributes :id, :user_id, :title, :detail, :start_time, :end_time, :place, :hostname, :start_time_for_display, :end_time_for_display #, :community_id, :is_private, :tag_names, :participation_users_name
 
-  has_many :participation_events
-  has_many :tag_events
+  # has_many :participation_events
+  # has_many :tag_events
 
   belongs_to :user
   # belongs_to :community
@@ -20,5 +20,15 @@ class EventSerializer < ActiveModel::Serializer
   def participation_users_name
     user_ids = object.participation_events.pluck(:user_id)
     return User.where(id: user_ids).pluck(:name)
+  end
+
+  def start_time_for_display
+    date_and_time = "#{object.start_time.month}月#{object.start_time.day}日 #{object.start_time.to_s[11, 2]}:#{object.start_time.to_s[14, 2]}"
+    return date_and_time
+  end
+
+  def end_time_for_display
+    date_and_time = "#{object.end_time.month}月#{object.end_time.day}日 #{object.end_time.to_s[11, 2]}:#{object.end_time.to_s[14, 2]}"
+    return date_and_time
   end
 end
