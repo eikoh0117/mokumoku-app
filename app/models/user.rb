77f0a3d7e288da1cdc8054d74bca13c94e05fd56
Hidemitsu Aoki <olympos0117@gmail.com>
2020-09-20 00:26:04 +0900

    email maximum 30

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_many :participation_communities, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: 10, minimum: 2}, uniqueness: true
-  validates :email, presence: true, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :email, presence: true, length: {maximum: 30, minimum: 2}, uniqueness: true
   validates :uid, presence: true
 end
