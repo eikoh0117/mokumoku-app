ccc00f4c4f90f258719eb2c0db38e912de61b63b
Hidemitsu Aoki <olympos0117@gmail.com>
2020-09-20 18:59:54 +0900

    Make it possible to fetch participation_events on index aciton of events_controller

--- app/controllers/v1/events_controller.rb
+++ app/controllers/v1/events_controller.rb
@@ -1,8 +1,15 @@
 class V1::EventsController < ApplicationController
   def index
     # events = Event.select(:id, :title, :detail, :start_time, :end_time, :place, :user_id)
-    events = Event.includes(:user)
-    render json: events
+    if params[:user_id]
+      participation_events = ParticipationEvent.where(user_id: params[:user_id])
+      participation_events_ids = participation_events.pluck(:event_id)
+      events = Event.where(id: participation_events_ids)
+      render json: events
+    else
+      events = Event.includes(:user)
+      render json: events
+    end
   end
   def create
     event = Event.new(event_params)
