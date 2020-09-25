127c1ef50e3c512c1672654ab3a00c4fd523bbb2
Hidemitsu Aoki <olympos0117@gmail.com>
2020-09-26 08:41:16 +0900

    Define update action of events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    resources :events, only: [:index, :create, :destroy]
+    resources :events, only: [:index, :create, :destroy, :update]
     resources :users, only: [:index, :create]
     resources :participation_events, only: [:create, :destroy, :index]
   end
